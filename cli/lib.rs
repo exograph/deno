5705ab9a1984fdd5916889590c0c2779b20cd14b
Luke Taylor <tekul@pm.me>
2023-07-20 17:55:32 +0100

    Add lib to cli (deno) crate
    We want to be able to call the cli code from within exograph so
    need more than a binary only package.

---
+++ cli/lib.rs
@@ -0,0 +1,43 @@
+pub mod args;
+pub mod auth_tokens;
+pub mod cache;
+pub mod deno_std;
+pub mod emit;
+pub mod errors;
+pub mod factory;
+pub mod file_fetcher;
+pub mod graph_util;
+pub mod http_util;
+pub mod js;
+pub mod lsp;
+pub mod module_loader;
+pub mod napi;
+pub mod node;
+pub mod npm;
+pub mod ops;
+pub mod resolver;
+pub mod standalone;
+pub mod tools;
+pub mod tsc;
+pub mod util;
+pub mod version;
+pub mod worker;
+
+pub use crate::args::flags_from_vec;
+pub use crate::args::DenoSubcommand;
+pub use crate::args::Flags;
+pub use crate::factory::CliFactory;
+pub use crate::resolver::CliGraphResolver;
+pub use crate::util::display;
+pub use crate::util::v8::get_v8_flags_from_env;
+pub use crate::util::v8::init_v8_flags;
+
+pub use args::CliOptions;
+pub use deno_core::anyhow::Context;
+pub use deno_core::error::AnyError;
+pub use deno_core::error::JsError;
+pub use deno_runtime::colors;
+pub use deno_runtime::fmt_errors::format_js_error;
+pub use std::env;
+pub use std::path::PathBuf;
+
