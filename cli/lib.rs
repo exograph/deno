696936188bbe14a990ed538877932251d1c4fe02
Luke Taylor <tekul@pm.me>
2023-07-20 17:55:32 +0100

    Add lib to cli (deno) crate
    We want to be able to call the cli code from within exograph so
    need more than a binary only package.

---
+++ cli/lib.rs
@@ -0,0 +1,110 @@
+pub mod args;
+pub mod auth_tokens;
+pub mod cache;
+pub mod cdp;
+pub mod deno_std;
+pub mod emit;
+pub mod errors;
+pub mod factory;
+pub mod file_fetcher;
+pub mod graph_util;
+pub mod http_util;
+pub mod js;
+pub mod lsp;
+pub mod module_loader;
+pub mod napi;
+pub mod node;
+pub mod npm;
+pub mod ops;
+pub mod resolver;
+pub mod standalone;
+pub mod tools;
+pub mod tsc;
+pub mod util;
+pub mod version;
+pub mod worker;
+
+pub use crate::args::flags_from_vec;
+pub use crate::args::DenoSubcommand;
+pub use crate::args::Flags;
+pub use crate::util::display;
+pub use crate::util::v8::get_v8_flags_from_env;
+pub use crate::util::v8::init_v8_flags;
+
+pub use deno_core::anyhow::Context;
+pub use deno_core::error::AnyError;
+pub use deno_core::error::JsError;
+pub use deno_core::futures::FutureExt;
+pub use deno_core::unsync::JoinHandle;
+pub use deno_runtime::colors;
+pub use deno_runtime::fmt_errors::format_js_error;
+pub use deno_runtime::tokio_util::create_and_run_current_thread_with_maybe_metrics;
+pub use factory::CliFactory;
+pub use std::env;
+pub use std::env::current_exe;
+pub use std::future::Future;
+pub use std::path::PathBuf;
+
+// NOTE(bartlomieju): keep IDs in sync with `runtime/90_deno_ns.js`.
+pub(crate) static UNSTABLE_GRANULAR_FLAGS: &[(
+  // flag name
+  &str,
+  // help text
+  &str,
+  // id to enable it in runtime/99_main.js
+  i32,
+)] = &[
+  (
+    deno_runtime::deno_broadcast_channel::UNSTABLE_FEATURE_NAME,
+    "Enable unstable `BroadcastChannel` API",
+    1,
+  ),
+  (
+    deno_runtime::deno_ffi::UNSTABLE_FEATURE_NAME,
+    "Enable unstable FFI APIs",
+    2,
+  ),
+  (
+    deno_runtime::deno_fs::UNSTABLE_FEATURE_NAME,
+    "Enable unstable file system APIs",
+    3,
+  ),
+  (
+    deno_runtime::deno_kv::UNSTABLE_FEATURE_NAME,
+    "Enable unstable Key-Value store APIs",
+    4,
+  ),
+  (
+    deno_runtime::deno_net::UNSTABLE_FEATURE_NAME,
+    "Enable unstable net APIs",
+    5,
+  ),
+  (
+    deno_runtime::ops::http::UNSTABLE_FEATURE_NAME,
+    "Enable unstable HTTP APIs",
+    6,
+  ),
+  (
+    deno_runtime::ops::worker_host::UNSTABLE_FEATURE_NAME,
+    "Enable unstable Web Worker APIs",
+    7,
+  ),
+  (
+    deno_runtime::deno_cron::UNSTABLE_FEATURE_NAME,
+    "Enable unstable Deno.cron API",
+    8,
+  ),
+];
+
+pub(crate) fn unstable_exit_cb(_feature: &str, api_name: &str) {
+  // TODO(bartlomieju): change to "The `--unstable-{feature}` flag must be provided.".
+  eprintln!("Unstable API '{api_name}'. The --unstable flag must be provided.");
+  std::process::exit(70);
+}
+  
+#[allow(dead_code)]
+pub(crate) fn unstable_warn_cb(feature: &str) {
+  eprintln!(
+    "The `--unstable` flag is deprecated, use --unstable-{feature} instead."
+  );
+}
